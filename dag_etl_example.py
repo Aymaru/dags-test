ba41ddfb0dec52f4aa9534dea2e1469c27963ff5
Aymaru <aycastillo@ic-itcr.ac.cr>
2021-01-18 14:01:14 -0600

    test

---
+++ dag_etl_example.py
@@ -0,0 +1,68 @@
+import json
+from datetime import timedelta
+
+from airflow.decorators import dag, task
+from airflow.utils.dates import days_ago
+
+# Default args for each task, args can be override on each task declaration
+default_args = {
+    'owner': 'smartnow',
+    'depends_on_past': False,
+    'email':['aymaru@smartnow.la'],
+    'email_on_failure':False,
+    'email_on_retry':False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=1),
+    'execution_time_out': timedelta(seconds=300)
+}
+
+# Using dag decorator to instantiate a DAG object, using the name function as unique id
+@dag(default_args=default_args, schedule_interval=timedelta(days=1), start_date=days_ago(2))
+def test_taskflow_api_etl():
+    """
+    ### TaskFlow API Example
+    """
+
+    # Using task decorator to instantiate a task, the operator is the function defined
+    @task()
+    def extract():
+        """
+        #### Extract task
+        """
+        data = '{"val_1": 231.52, "val_2": 675.52, "val_3": 347.98}'
+
+        data_dict = json.loads(data)
+        return data_dict
+
+    # Using task decorator to instantiate a task, passing multiple_outputs params that were not define in default args
+    @task(multiple_outputs=True)
+    def transform(data_dict: dict):
+        """
+        #### Transform task
+        """
+        total_order_value = 0
+        total_orders = 0
+
+
+        for values in data_dict.values():
+            total_order_value += values
+            total_orders+=1
+
+        return {"total_orders": total_orders, "total_order_value": total_order_value}
+
+    @task()
+    def load(total_orders: int, total_order_value: float):
+        """
+        #### Load task
+        """
+
+        print("Total orders processed: ", total_orders)
+        print("Total order value is: ", total_order_value)
+
+    data = extract()
+
+    summary = transform(data)
+
+    load(summary["total_orders"],summary["total_order_value"])
+
+test_etl_dag = test_taskflow_api_etl()
---
+++ dag_example.py
@@ -0,0 +1,69 @@
+
+from datetime import timedelta
+from random import randrange
+from time import sleep
+
+# Import the DAG object, used to instantiate a dag
+from airflow import DAG
+
+# Import the Operators, needed to operate
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+from airflow.utils.dates import days_ago
+
+# Default arguments that will get passed on to each operator
+# These arguments can be override on each operator initialization
+default_args = {
+    'owner': 'smartnow',
+    'depends_on_past': False,
+    'retries': 2,
+    'retry_delay': timedelta(minutes=2)
+}
+
+# DAG initialization
+dag = DAG(
+    'python_example',
+    default_args=default_args,
+    description='A simple dag example using python operator and bash operator',
+    schedule_interval=timedelta(days=1), #Set to run everyday
+    start_date=days_ago(2),
+    tags=['example']
+)
+
+# Function executed by the PythonOperator on First Task
+def generate_value():
+    rand_num = randrange(10)
+    return rand_num
+
+# Task declaration using PythonOperator
+first_task = PythonOperator(
+    task_id='generate_value',
+    python_callable=generate_value,
+    dag=dag # task assign to dag
+)
+
+# Task declaration using BashOperator
+last_task = BashOperator(
+    task_id = 'print_date',
+    bash_command='date', # this is the bash command executed
+    dag=dag
+)
+
+#Function executed by PythonOperator, just sleep for a few seconds each
+def sleep_function(sleep_time: int):
+    print('Sleeping for',sleep_time,' seconds')
+    sleep(sleep_time)
+    return True
+
+# Declaring multiple tasks
+for i in range(0,3):
+    sleep_time = randrange(1,8)
+    task = PythonOperator(
+        task_id='sleep_'+str(i), #dynamic generating the task id
+        python_callable=sleep_function,
+        op_kwargs={'sleep_time':sleep_time}, # Passing parameters on to the python function
+        dag=dag
+    )
+
+    first_task >> task >> last_task #Setting the tasks dependencies.
+
