31d58e73d72e549867bcfcf867c6de00f7bb9568
Aymaru <aycastillo@ic-itcr.ac.cr>
2021-01-22 16:43:37 -0600

    test postgresql n?

--- dags/dag_postgresql.py
+++ dags/dag_postgresql.py
@@ -33,11 +33,9 @@ def access_database():
     cursor = connection.cursor()
     cursor.execute(request)
     sources = cursor.fetchall()
-    print('result: ' + sources)
+
     for source in sources:
-        print(source[0])
-        print(source[1])
-        print(source[2])
+        print(source)
     return sources
 
 hooktask = PythonOperator(
