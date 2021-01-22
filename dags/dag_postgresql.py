1a49732e0a87362f5ae7595aa96156477f1ca232
Aymaru <aycastillo@ic-itcr.ac.cr>
2021-01-22 16:24:20 -0600

    test postgresql n?

--- dags/dag_postgresql.py
+++ dags/dag_postgresql.py
@@ -24,7 +24,7 @@ dag = DAG(
 
 
 def access_database():
-    request = "SELECT * FROM public.company;"
+    request = "SELECT * FROM company;"
     #  request = "select * from pg_tables where schemaname='public';"
     #"SELECT * FROM information_schema.tables WHERE table_schema = 'public'"
     #"SELECT * FROM public.company;" #"SELECT schema_name FROM information_schema.schemata;"
