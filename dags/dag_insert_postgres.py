9fd935dcf89d8a04c8c3e4abd81d61c2be7e116f
Aymaru <aycastillo@ic-itcr.ac.cr>
2021-01-25 10:23:46 -0600

    dag insert postgres

---
+++ dags/dag_insert_postgres.py
@@ -0,0 +1,32 @@
+from datetime import datetime, timedelta
+from random import randrange
+from airflow import DAG
+from airflow.hooks.postgres_hook import PostgresHook
+from airflow.operators.python_operator import PythonOperator
+from airflow.utils.dates import days_ago
+
+default_args = {
+    'owner': 'smartnow',
+    'depends_on_past': False,
+    'retries': 2,
+    'retry_delay': timedelta(seconds=30)
+}
+
+with DAG( 'postgresql_insert_example', default_args=default_args, description='A simple dag example using postgresql to insert into a table', schedule_interval=timedelta(days=1), start_date=days_ago(2), tags=["example","insert","etl"] ) as dag :
+    src = PostgresHook(postgres_conn_id='mypsql',schema='postgres')
+    src_conn = src.get_conn()
+    cursor = src_conn.cursor()
+
+    names = ['Paul', 'Andres', 'Aymaru','Julio','Stward']
+    total_inserts = randrange(5,20)
+
+    for i in range(0,total_inserts):
+        rand_name = randrange(0,len(names))
+        rand_amount = randrange(10000,500000)
+
+        cursor.execute ("INSERT sales(name,amount) VALUES ( %(name)s, %(amount)d )", {'name':names[rand_name],'amount':rand_amount})
+
+    
+
+
+
