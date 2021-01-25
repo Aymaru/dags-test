7ea8ec66a6155fe22dcc5b94b582956274d074da
Aymaru <aycastillo@ic-itcr.ac.cr>
2021-01-25 11:21:02 -0600

    insert postgres

---
+++ dags/dag_insert_postgres-bak.py
@@ -0,0 +1,33 @@
+from datetime import datetime, timedelta
+from random import randrange
+from airflow import DAG
+from airflow.hooks.postgres_hook import PostgresHook
+from airflow.operators.python_operator import PythonOperator
+from airflow.utils.dates import days_ago
+from airflow.models.baseoperator import BaseOperator
+from airflow.utils.decorators import apply_defaults
+from dags.operators import SalesInsertOperator
+
+default_args = {
+    'owner': 'smartnow',
+    'depends_on_past': False,
+    'retries': 2,
+    'retry_delay': timedelta(seconds=30)
+}
+
+# with DAG( 'postgresql_insert_example', default_args=default_args, description='A simple dag example using postgresql to insert into a table', schedule_interval=timedelta(days=1), start_date=days_ago(2), tags=["example","insert","etl"] ) as dag :
+#     src = PostgresHook(postgres_conn_id='mypsql',schema='postgres')
+#     src_conn = src.get_conn()
+#     cursor = src_conn.cursor()
+
+#     names = ['Paul', 'Andres', 'Aymaru','Julio','Stward']
+#     total_inserts = randrange(5,20)
+
+#     for i in range(0,total_inserts):
+#         rand_name = randrange(0,len(names))
+#         rand_amount = randrange(10000,500000)
+
+#         cursor.execute ("INSERT INTO sales(name,amount) VALUES ( %(name)s, %(amount)s )", {'name':names[rand_name],'amount':rand_amount})
+with DAG( 'postgresql_insert_example', default_args=default_args, description='A simple dag example using postgresql to insert into a table', schedule_interval=timedelta(days=1), start_date=days_ago(2), tags=["example","insert","etl"] ) as dag :
+    total_inserts = randrange(5,20)
+    insert_task = InsertSalesOperator(postgres_conn_id='mypsql',database='postgres',inserts=total_inserts)
--- dags/dag_insert_postgres.py
+++ dags/dag_insert_postgres.py
@@ -15,19 +15,35 @@ default_args = {
     'retry_delay': timedelta(seconds=30)
 }
 
-# with DAG( 'postgresql_insert_example', default_args=default_args, description='A simple dag example using postgresql to insert into a table', schedule_interval=timedelta(days=1), start_date=days_ago(2), tags=["example","insert","etl"] ) as dag :
-#     src = PostgresHook(postgres_conn_id='mypsql',schema='postgres')
-#     src_conn = src.get_conn()
-#     cursor = src_conn.cursor()
+dag = DAG( 'postgresql_insert_example', 
+    default_args=default_args, 
+    description='A simple dag example using postgresql to insert into a table', 
+    schedule_interval=timedelta(days=1), 
+    start_date=days_ago(2), 
+    tags=["example","insert","etl"] ) 
 
-#     names = ['Paul', 'Andres', 'Aymaru','Julio','Stward']
-#     total_inserts = randrange(5,20)
+    #total_inserts = randrange(5,20)
+    #insert_task = InsertSalesOperator(postgres_conn_id='mypsql',database='postgres',inserts=total_inserts)
 
-#     for i in range(0,total_inserts):
-#         rand_name = randrange(0,len(names))
-#         rand_amount = randrange(10000,500000)
+def insert_values_sales():
 
-#         cursor.execute ("INSERT INTO sales(name,amount) VALUES ( %(name)s, %(amount)s )", {'name':names[rand_name],'amount':rand_amount})
-with DAG( 'postgresql_insert_example', default_args=default_args, description='A simple dag example using postgresql to insert into a table', schedule_interval=timedelta(days=1), start_date=days_ago(2), tags=["example","insert","etl"] ) as dag :
+    names = ['Paul', 'Andres', 'Aymaru','Julio','Stward']
     total_inserts = randrange(5,20)
-    insert_task = InsertSalesOperator(postgres_conn_id='mypsql',database='postgres',inserts=total_inserts)
+
+    src = PostgresHook(postgres_conn_id='mypsql',schema='postgres')
+    src_conn = src.get_conn()
+    cursor = src_conn.cursor()    
+
+    for i in range(0,self.inserts):
+        rand_name = randrange(0,len(names))
+        rand_amount = randrange(10000,500000)
+
+        cursor.execute ("INSERT INTO sales(name,amount) VALUES ( %(name)s, %(amount)s )", {'name':names[rand_name],'amount':rand_amount})
+    cursor.commit()
+    cursor.close()
+
+hook_task = PythonOperator(
+    task_id='insertIntoSales',
+    python_callable=insert_values_sales,
+    dag=dag
+)
